2f491919b5c80a5109b56e0560ffe93a477a0817
Cassie Achzenick <caachzenick@gmail.com>
2020-03-30 09:21:22 -0600

    Add feature test

---
+++ spec/features/user_can_search_food_by_criteria_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'User' do
+  it 'user sees search field when visiting home page' do
+
+    visit '/'
+
+    fill_in 'search', with: 'sweet potatoes'
+
+    click_on 'search'
+
+    expect(current_page).to eq("/foods")
+
+    expect(page).to have_content("Total Items Returned: 10")
+    expect(page).to have_css(".food", count: 10)
+
+    within(first(".food")) do
+      expect(page).to have_css(".GTIN/UPC code")
+      expect(page).to have_css(".description")
+      expect(page).to have_css(".Brand Owner")
+      expect(page).to have_css(".ingredients")
+    end
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+# And for each of the foods I should see:
+# - The food's GTIN/UPC code
+# - The food's description
+# - The food's Brand Owner
+# - The food's ingredients
