c228bd3fd3bc77eee9cf17fe9dfdbd22265f6144
Cassie Achzenick <caachzenick@gmail.com>
2020-03-30 10:08:22 -0600

    Added figaro and stored api key safely

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
   def index
     search_criteria = params["q"]
 
-    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/search?api_key=4A0mVGk5VY9uktsEpGZmWZhWdjE8QEVv7w5dFL04&ingredients=#{search_criteria}")
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/search?api_key=#{ENV["USDA_API_KEY"]}&ingredients=#{search_criteria}")
 
     response = conn.get
 
