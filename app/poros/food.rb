fdaacb9953dcfb62575e3a685daa644e3b922f71
Cassie Achzenick <caachzenick@gmail.com>
2020-03-30 10:41:44 -0600

    Add food poro test

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -2,7 +2,6 @@ class Food
   attr_reader :upc_code, :description, :brand_owner, :ingredients
 
   def initialize(params)
-    require "pry"; binding.pry
     @upc_code = params[:gtinUpc]
     @description = params[:description]
     @brand_owner = params[:brandOwner]
--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -5,6 +5,11 @@ describe 'Food' do
     it "supplies the necessary information for the food object" do
       params = {gtinUpc: "728229015529", description: "REAL VEGETABLE CHIPS", brandOwner: "The Hain Celestial Group, Inc.", ingredients: "A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA)"}
       food = Food.new(params)
+
+      expect(food.brand_owner).to eq("The Hain Celestial Group, Inc.")
+      expect(food.upc_code).to eq("728229015529")
+      expect(food.description).to eq("REAL VEGETABLE CHIPS")
+      expect(food.ingredients).to eq("A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA)")
     end
   end
 end
