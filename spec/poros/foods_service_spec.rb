6f4b1549612f506dccb01954925201999a7ba613
Cassie Achzenick <caachzenick@gmail.com>
2020-03-30 10:47:22 -0600

    Add foods service test

---
+++ spec/poros/foods_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'FoodService' do
+  describe 'get_foods' do
+    it "makes an api call and stores the responses as food objects" do
+      query_params = "sweet potatoes"
+      conn = FoodsService.new
+      foods = conn.get_foods(query_params)
+
+      expect(foods.count).to eq(50)
+      expect(conn.all_foods.count).to eq(50)
+      expect(conn.all_foods.first).to be_a(Food)
+    end
+  end
+end
