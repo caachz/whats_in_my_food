b9bd6b1663b7932cc71237e2db32869028fd1720
Cassie Achzenick <caachzenick@gmail.com>
2020-03-30 09:51:52 -0600

    Add faraday api connection

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,8 @@
 class FoodsController < ApplicationController
   def index
+    search_criteria = params["q"]
+
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/search?api_key=4A0mVGk5VY9uktsEpGZmWZhWdjE8QEVv7w5dFL04&ingredients=#{search_criteria}")
     require "pry"; binding.pry
   end
 end
---
+++ app/poros/FoodsService.rb
@@ -0,0 +1,5 @@
+class FoodsService
+  def initialize
+    require "pry"; binding.pry
+  end
+end
